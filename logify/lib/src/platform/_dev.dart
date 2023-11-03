268a7d881fdc870cd79c3e32485b78d077ba4b8c
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-11-03 10:20:25 -0300

    Refact Stdout implementations

---
+++ logify/lib/src/platform/_dev.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'dart:developer' as dev show log;
+
+import 'stdio.dart';
+
+typedef DevLog = void Function(
+  String message, {
+  DateTime? time,
+  int? sequenceNumber,
+  int level,
+  String name,
+  Zone? zone,
+  Object? error,
+  StackTrace? stackTrace,
+});
+
+/// A stdout that prints to the console.
+/// It uses the `dart:developer` library.
+class DevStdout implements Stdout {
+  const DevStdout([this.log = dev.log]);
+
+  final DevLog log;
+
+  @override
+  final bool hasTerminal = false;
+
+  @override
+  final bool supportsAnsiEscapes = true;
+
+  @override
+  int get terminalColumns =>
+      throw const StdoutException('terminalColumns is not supported');
+
+  @override
+  void writeln(String message) => log(message);
+}
--- logify/lib/src/platform/io.dart
+++ logify/lib/src/platform/io.dart
@@ -1,4 +1,5 @@
+export '_dev.dart';
 export '_multiplatform.dart'
     if (dart.library.html) '_web.dart'
-    if (dart.library.mirrors) '_cmdline.dart';
+    if (dart.library.mirrors) '_cli.dart';
 export 'stdio.dart';
