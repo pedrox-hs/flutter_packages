6ec9a5238b4fd70e06f978051bb5ee4de76fb3b7
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2024-01-01 18:01:49 -0300

    Add client extensions tests
    - some refactor

--- shic/lib/src/ext/client.dart
+++ shic/lib/src/ext/client.dart
@@ -1,16 +1,16 @@
 import 'package:http/http.dart';
 
 extension ClientExt on Client {
-  Future<StreamedResponse> upload(
+  Future<Response> upload(
     Uri url,
     Iterable<MultipartFile> files, {
     String method = 'POST',
     Map<String, String> fields = const <String, String>{},
-  }) {
+  }) async {
     final request = MultipartRequest(method, url)
       ..files.addAll(files)
       ..fields.addAll(fields);
 
-    return send(request);
+    return Response.fromStream(await send(request));
   }
 }
